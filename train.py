e51cb5cef66f24525a8ab05c6a9c43da30de9492
Pablo Ruiz Ponce <pabloruizp00@gmail.com>
2022-07-25 14:36:09 +0200

    Training Arguments

--- train.py
+++ train.py
@@ -1,20 +1,24 @@
 import torchvision
 import torch
 import os
+import utils.logger
+import argparse
+from random import randint
 from torch.utils.data import DataLoader
 from utils.loss import YOLOLoss
 from models.vgg16 import YOLOVGG16
 from data.datasets import COCODataset
 from torchsummary import summary
-import utils.logger
 from tqdm import tqdm
+
 
 # Using Apple Metal to accelerate the training
 # The operator 'aten::_slow_conv2d_forward' is not current implemented for the MPS device.
 # Check in https://github.com/pytorch/pytorch/issues/77764
 # device = "mps" if torch.backends.mps.is_available() else "cpu"
 
-logger = utils.logger.Logger('YOLOv1')
+
+# Default values
 device = "cuda" if torch.cuda.is_available() else "cpu"
 batch_size = 16
 epochs = 10
@@ -22,8 +26,14 @@ lr = 1e-4
 weight_decay = 0.0005
 images_path = "/Users/pabloruizponce/Downloads/val2017"
 annotations_path = "/Users/pabloruizponce/Downloads/annotations/instances_val_reduced.json"
+log = False
 
-
+parser = argparse.ArgumentParser(description='Training arguments')
+parser.add_argument("--batch-size", "-b", type=int, help="Batch size")
+parser.add_argument("--epochs", "-e", type=int, help="Number of epochs")
+parser.add_argument("--images", "-i", type=int, help="Path of the input images")
+parser.add_argument("--annotations", "-a", type=int, help="Path of the expected annotations")
+parser.add_argument("--logger", "-l", action="store_true", help="Log the training data")
 
 
 train_dataset = COCODataset(annotations_path, 
@@ -41,25 +51,15 @@ model = YOLOVGG16(classes=3).to(device)
 loss_fn = YOLOLoss
 optimizer = torch.optim.SGD(model.head.parameters(), lr=lr, weight_decay=weight_decay, momentum=0.9)
 
-
-
 # 1e-3 = 1e-4 * X^50   -> X = 10 ** (1./50.)
 scheduler1 = torch.optim.lr_scheduler.StepLR(optimizer=optimizer,gamma=(10 ** (1./50.)),step_size=1)
 scheduler2 = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer,gamma=0.1,milestones=[75,105,135])
 scheduler = torch.optim.lr_scheduler.SequentialLR(optimizer=optimizer, schedulers=[scheduler1,scheduler2], milestones=[50])
 
 
-logger.config = {
-  "learning_rate": lr,
-  "weight_decay": weight_decay,
-  "epochs": epochs,
-  "batch_size": batch_size
-}
-
-logger.watch(model)
 
 
-def train(dataloader, model, loss_fn, optimizer, epoch_n=0):
+def train(dataloader, model, loss_fn, optimizer, epoch_n=0, logger=None):
     size = len(dataloader)
     model.train()
 
@@ -87,26 +87,70 @@ def train(dataloader, model, loss_fn, optimizer, epoch_n=0):
 
             tepoch.set_postfix(loss=e_loss[0].item(),lr=scheduler.get_last_lr()[0])
 
-        logger.log(loss=e_loss[0],
-                   xy_loss=e_loss[1], 
-                   wh_loss=e_loss[2], 
-                   obj_loss=e_loss[3], 
-                   noobj_loss=e_loss[4], 
-                   class_loss=e_loss[5],
-                   lr=scheduler.get_last_lr()[0])
+        if logger != None:
+            logger.log(loss=e_loss[0],
+                    xy_loss=e_loss[1], 
+                    wh_loss=e_loss[2], 
+                    obj_loss=e_loss[3], 
+                    noobj_loss=e_loss[4], 
+                    class_loss=e_loss[5],
+                    lr=scheduler.get_last_lr()[0])
 
 
 
 
 if __name__ == "__main__":
+
+    args = parser.parse_args()
+
+    if args.batch_size != None:
+        batch_size = args.batch_size
+    if args.epochs != None:
+        epochs = args.epochs
+    if args.images != None:
+        images_path = args.images
+    if args.annotations != None:
+        annotations_path = args.annotations
+    if args.logger != None:
+        log = args.logger
+
     summary(model, (3, 416, 416))
+    print("Epochs: ", epochs)
+    print("Batch Size: ", batch_size)
+    print("Images Path: ", images_path)
+    print("Annotations Path: ", annotations_path)
+    print("----------------------------------------------------------------")
+
+    logger = None
+
+    if log:
+        logger = utils.logger.Logger('YOLOv1')
+        
+        logger.config = {
+        "learning_rate": lr,
+        "weight_decay": weight_decay,
+        "epochs": epochs,
+        "batch_size": batch_size
+        }
+
+        logger.watch(model)
+    else:
+        run_name = str(randint(10000,99999))
 
     if not os.path.exists("./YOLOoutputs"):
         os.makedirs("./YOLOoutputs")
 
-    os.makedirs("./YOLOoutputs/" + logger.name)
+    os.makedirs("./YOLOoutputs/" + (logger.name if logger != None else run_name))
 
     for e in range(epochs):
-        train(train_dataloader, model, loss_fn, optimizer, epoch_n=e+1)
-        torch.save(model.state_dict(), "./YOLOoutputs/" + logger.name + "/epoch-" + str(e+1) + ".pth")
+        train(train_dataloader, 
+              model, 
+              loss_fn, 
+              optimizer, 
+              epoch_n=e+1, 
+              logger=(logger if logger != None else None))
+
+        torch.save(model.state_dict(), 
+                   "./YOLOoutputs/" + (logger.name if logger != None else run_name) + "/epoch-" + str(e+1) + ".pth")
+
         scheduler.step()
